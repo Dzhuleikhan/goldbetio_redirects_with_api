0a77d3ca6f73d1695b8327f37f20cf931d737132
dzhuleikhan <dzhuleikhan@gmail.com>
2025-03-06 16:21:36 +0530

    4

--- bonus/index.php
+++ bonus/index.php
@@ -29,7 +29,8 @@ function fetchDomain() {
     }
     
     // API endpoint with countryCode parameter
-    $apiUrl = "https://gbetauth.com/api/v2/rotator/available-domain?country=" . urlencode($countryCode);
+    // $apiUrl = "https://gbetauth.com/api/v2/rotator/available-domain?country=" . urlencode($countryCode);
+    $apiUrl = "https://gbetauth.com/api/v2/rotator/available-domain?country=" . $countryCode;
     // $apiUrl = "https://gbetauth.com/api/v2/rotator/available-domain?country=AZ";
     
     $ch = curl_init();
